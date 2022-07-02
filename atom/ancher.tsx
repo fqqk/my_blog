5ccc5ff1fe80219c0ef493148fcd9eeb39c23a54
fqqk <fqqk.work68@gmail.com>
2022-07-02 15:54:20 +0900

    Headerコンポーネント80%

---
+++ atom/ancher.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+type Props = {
+  children: string;
+  path: string;
+};
+
+const Ancher = ({ children, path }: Props) => {
+  return (
+    <Link href={path}>
+      <a className="font-bold font-sans text-lg hover:text-pink-500">
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+export default Ancher;
--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
-import Link from 'next/link'
+import Ancher from "atom/ancher";
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
-    </h2>
-  )
-}
+    <div className="fixed w-full flex-1 bg-white">
+      <nav className="w-11/12 border-b-2 border-gray-300 m-auto mb-30">
+        <div className="space-x-10 flex-1 px-48 py-6">
+          <Ancher path="/">Home</Ancher>
+          <Ancher path="#">Profile</Ancher>
+          <Ancher path="#">Dev</Ancher>
+          <Ancher path="#">Posts</Ancher>
+          <Ancher path="#">Want</Ancher>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
