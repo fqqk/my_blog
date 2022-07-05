c6ca1b85986063db4c7df2bc37180f633e6b44fb
fqqk <fqqk.work68@gmail.com>
2022-07-05 12:05:11 +0900

    footerレイアウトデザイン済

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,35 @@
 import Container from "./container";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+    <footer className="bg-neutral-50 ">
+      <div className="w-11/12 m-auto border-t-2 border-neutral-200">
+        <div className="py-8 flex flex-col lg:flex-row items-center justify-between">
+          <Link href="/">
+            <a className="border-b-2 border-black m-auto hover:text-pink-500 duration-200 hover:border-pink-500 leading-none">
+              <p className="">back to top</p>
+            </a>
+          </Link>
+
+          <div className="flex  lg:flex-row  items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="https://twitter.com/fqqk_t0ma"
+              target="blank"
+              className=" hover:text-pink-500 font-semibold  py-3 px-12  duration-200"
             >
-              Read Documentation
+              Twitter
+            </a>
+            <a
+              href="https://github.com/fqqk"
+              target="blank"
+              className=" hover:text-pink-500 font-semibold  py-3 px-12  duration-200"
+            >
+              Github
             </a>
           </div>
         </div>
-      </Container>
+      </div>
     </footer>
   );
 };
