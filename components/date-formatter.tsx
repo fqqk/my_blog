361872d50371195e347a7d0a987fda7c3bad5010
fqqk <fqqk.work68@gmail.com>
2022-08-22 23:05:33 +0900

    日付デザイン変更

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -7,7 +7,7 @@ type Props = {
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
 
-  return <time dateTime={dateString}>{format(date, "yyyy/MM/d HH:mm")}</time>;
+  return <time dateTime={dateString}>{format(date, "yyyy.MM.d HH:mm")}</time>;
 };
 
 export default DateFormatter;
