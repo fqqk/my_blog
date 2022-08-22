95a6f25e44058b06b1ea4a451d43b01a94df2ba1
fqqk <fqqk.work68@gmail.com>
2022-08-22 22:54:13 +0900

    タグリンク追加

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,27 +1,37 @@
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import PostTitle from "./post-title";
+import Link from "next/link";
 
 type Props = {
   title: string;
-  coverImage: string;
   created_at: string;
   updated_at: string;
+  categories: string[];
 };
 
-const PostHeader = ({ title, coverImage, created_at, updated_at }: Props) => {
+const PostHeader = ({ title, created_at, updated_at, categories }: Props) => {
   return (
     <>
       <div className="w-3/5 mx-auto">
         <PostTitle>{title}</PostTitle>
-        <div>
-          <div className=" text-gray-400">
-            公開日:
-            <DateFormatter dateString={created_at} />
+        <div className="flex justify-between">
+          <div>
+            <Link href={`/categories/${categories}`}>
+              <a className="bg-pink-500 rounded-full p-2 hover:bg-pink-300 text-white duration-200 ml-3 text-sm">
+                {categories}
+              </a>
+            </Link>
           </div>
-          <div className=" text-gray-400">
-            最終更新日:
-            <DateFormatter dateString={updated_at} />
+          <div>
+            <div className="text-gray-400 text-right">
+              公開日:
+              <DateFormatter dateString={created_at} />
+            </div>
+            <div className="text-gray-400 text-right">
+              最終更新日:
+              <DateFormatter dateString={updated_at} />
+            </div>
           </div>
         </div>
       </div>
