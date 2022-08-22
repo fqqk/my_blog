e55a8ff5d51b49ee57e4a36c1b9c90f3f72fa8fd
fqqk <fqqk.work68@gmail.com>
2022-08-22 22:54:02 +0900

    タグリンク追加

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,7 +1,6 @@
 import DateFormatter from "./date-formatter";
 import CoverImage from "./cover-image";
 import Link from "next/link";
-import Author from "../types/author";
 
 type Props = {
   title: string;
@@ -9,6 +8,7 @@ type Props = {
   created_at: string;
   excerpt: string;
   slug: string;
+  categories: string[];
 };
 
 const PostPreview = ({
@@ -17,13 +17,19 @@ const PostPreview = ({
   created_at,
   excerpt,
   slug,
+  categories,
 }: Props) => {
   return (
     <div className="w-4/6 mx-auto my-10">
       <div className="text-lg">
         <DateFormatter dateString={created_at} />
+        <Link href={`/categories/${categories}`}>
+          <a className="bg-pink-500 rounded-full p-2 hover:bg-pink-300 text-white duration-200 ml-3 text-sm">
+            {categories}
+          </a>
+        </Link>
       </div>
-      <h3 className="text-3xl mb-3 leading-snug text-pink-500 font-bold">
+      <h3 className="text-3xl mb-3 mt-1 leading-snug text-pink-500 font-bold">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="underline">{title}</a>
         </Link>
