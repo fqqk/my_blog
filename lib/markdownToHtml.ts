d93f6dae38f455a1bdfc3645caa3b77386154c66
fqqk <fqqk.work68@gmail.com>
2022-08-22 22:50:09 +0900

    highlight.jsにてシンタックスハイライト導入

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,15 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { unified } from "unified";
+import highlight from "remark-highlight.js";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(highlight)
+    .use(rehypeStringify)
+    .process(markdown);
+  return result.toString();
 }
