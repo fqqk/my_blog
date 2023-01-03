92755e7d87318077619540b323b1d327a2c6e3ef
fqqk <fqqk.work68@gmail.com>
2023-01-03 15:15:40 +0900

    customezed screen,fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,71 @@
 module.exports = {
-  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  content: ["./atom/**/*.tsx", "./components/**/*.tsx", "./pages/**/*.tsx"],
   theme: {
+    screens: {
+      // --------------------------------------------------
+      // custom settings
+      // --------------------------------------------------
+      sp: "350px",
+      tab: "520px",
+      pc: "1024px",
+      // --------------------------------------------------
+      // default settings
+      // --------------------------------------------------
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontSize: {
+      xxs: [
+        "0.5rem",
+        {
+          lineHeight: "0.75rem",
+        },
+      ],
+      xs: [
+        "0.75rem",
+        {
+          lineHeight: "1rem",
+        },
+      ],
+      sm: [
+        "0.875rem",
+        {
+          lineHeight: "1.25rem",
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.5rem",
+        },
+      ],
+      lg: [
+        "1.125rem",
+        {
+          lineHeight: "1.75rem",
+        },
+      ],
+      xl: [
+        "1.25rem",
+        {
+          lineHeight: "1.75rem",
+        },
+      ],
+      "2xl": [
+        "1.5rem",
+        {
+          lineHeight: "2rem",
+        },
+      ],
+      "3xl": [
+        "1.875rem",
+        {
+          lineHeight: "2.25rem",
+        },
+      ],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
