0f671cfe72816e7db55703e506c73cc63cc95e11
fqqk <fqqk.work68@gmail.com>
2023-01-09 17:51:30 +0900

    rename component

---
+++ atom/PostAncher.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+type Props = {
+  children: string;
+  path: string;
+};
+
+const PostAncher = ({ children, path }: Props) => {
+  return (
+    <Link href={path}>
+      <a className="font-sans font-bold hover:text-pink-500 duration-200 sp:text-xs tab:text-sm pc:text-lg">
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+export default PostAncher;
