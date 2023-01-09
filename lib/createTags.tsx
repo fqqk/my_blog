444e43a8e6b2efd32ccde3273e6ce2eeafcbdf88
fqqk <fqqk.work68@gmail.com>
2023-01-09 17:40:14 +0900

    create but not use fn

--- lib/createTags.tsx
+++ lib/createTags.tsx
@@ -5,12 +5,13 @@ import { SiTypescript } from "react-icons/si";
 import { SiRubyonrails } from "react-icons/si";
 import { SiReact } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { SiCss3 } from "react-icons/si";
 
 export function createTags(categories: string[]) {
+  type icon = JSX.Element | null;
+  let icon;
   const tags = categories.map((category) => {
-    type icon = JSX.Element | null;
-    let icon;
-
     switch (category) {
       case "react":
         icon = (
@@ -76,17 +77,41 @@ export function createTags(categories: string[]) {
           </IconContext.Provider>
         );
         break;
+      case "tailwind":
+        icon = (
+          <IconContext.Provider
+            value={{
+              color: "#ffffff",
+              className:
+                "inline sp:w-3.5 sp:h-3.5 tab:w-4 tab:h-4 pc:w-4 pc:h-4",
+            }}
+          >
+            <SiTailwindcss />
+          </IconContext.Provider>
+        );
+        break;
+      case "css":
+        icon = (
+          <IconContext.Provider
+            value={{
+              color: "#ffffff",
+              className:
+                "inline sp:w-3.5 sp:h-3.5 tab:w-4 tab:h-4 pc:w-4 pc:h-4",
+            }}
+          >
+            <SiCss3 />
+          </IconContext.Provider>
+        );
+        break;
       default:
         icon = null;
     }
     return (
-      <>
-        <Link href={`/categories/${category}`}>
-          <a className="bg-pink-500 rounded-full hover:bg-pink-300 text-white duration-200 ml-3 sp:text-xxs sp:p-1.5 tab:text-xs pc:text-sm pc:p-2">
-            {icon ? icon : category}
-          </a>
-        </Link>
-      </>
+      <Link href={`/categories/${category}`}>
+        <a className="bg-pink-500 rounded-full hover:bg-pink-300 text-white duration-200 ml-3 sp:text-xxs sp:p-1.5 tab:text-xs pc:text-sm pc:p-2">
+          {icon ? icon : category}
+        </a>
+      </Link>
     );
   });
 
