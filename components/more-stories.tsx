0d96c44e84e94d46d7a2e45d5a76079044e52106
fqqk <fqqk.work68@gmail.com>
2023-01-09 17:53:39 +0900

    add year archive section

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -1,26 +1,56 @@
 import PostPreview from "./post-preview";
 import Post from "../types/post";
+import ArchiveSection from "./ArchiveSection";
 
 type Props = {
   posts: Post[];
 };
 
+type YearPostDataType = {
+  year: string;
+  year_posts: Post[];
+};
+
 const MoreStories = ({ posts }: Props) => {
-  return (
-    <section className="pt-10">
-      <div className="my-20">
-        {posts.map((post) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            created_at={post.created_at}
-            slug={post.slug}
-            categories={post.categories}
-          />
-        ))}
-      </div>
-    </section>
+  // Note: 'yyyy-mm-dd -> yyyy'
+  const created_at_list = posts.map((post) => post.created_at.slice(0, 4));
+  // Note: データの重複をなくし、年配列を作成
+  // Memo: posts/indexでも使用しているので後ほど共通化
+  const years = created_at_list.filter(
+    (element, index) => created_at_list.indexOf(element) === index
   );
+  const post_data_by_year_group: YearPostDataType[] = [];
+  years.forEach((year, index) => {
+    const postsData = posts.filter((i) => i.created_at.slice(0, 4) === year);
+    const post = {
+      year: year,
+      year_posts: postsData,
+    };
+    post_data_by_year_group[index] = post;
+  });
+
+  const yearPostList = post_data_by_year_group.map((post_data) => {
+    const postList = post_data.year_posts.map((post) => {
+      return (
+        <PostPreview
+          key={post.slug}
+          title={post.title}
+          created_at={post.created_at}
+          slug={post.slug}
+          categories={post.categories}
+        />
+      );
+    });
+
+    return (
+      <>
+        <ArchiveSection year={post_data.year} />
+        {postList}
+      </>
+    );
+  });
+
+  return <div className="my-20">{yearPostList}</div>;
 };
 
 export default MoreStories;
