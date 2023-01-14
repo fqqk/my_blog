22ed5e99024ea44ce9a5788a3e22931777d5c80a
fqqk <fqqk.work68@gmail.com>
2023-01-14 17:36:47 +0900

    fix meta

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { BLOG_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
 const Meta = () => {
   return (
@@ -13,13 +13,13 @@ const Meta = () => {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/fqqk_favicon-32x32.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/fqqk_favicon-16x16.png"
+        href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
@@ -32,7 +32,10 @@ const Meta = () => {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta name="description" content={BLOG_NAME} />
+      <meta
+        name="description"
+        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+      />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
