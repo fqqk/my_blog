de90d2f3c405f271409d5c3ca454d34e90fefe5d
fqqk <fqqk.work68@gmail.com>
2023-02-06 08:05:30 +0900

    remove react-scroll and react react-dom change ver 17.0.2

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,26 @@
 import Header from "components/header";
 import Footer from "./footer";
 import Meta from "./meta";
+import { useRef } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
+  const scrollTopRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
       <Meta />
       <Header />
       <div
         className="min-h-screen w-11/12 m-auto border-pink-400 border-x-2"
-        id="top-of-scroll"
+        ref={scrollTopRef}
       >
         <main>{children}</main>
       </div>
-      <Footer />
+      <Footer targetEl={scrollTopRef}/>
     </>
   );
 };
